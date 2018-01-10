0baeb5934eafb5899f7654efa4cba61ed0a5cd76
moekaz <mohamed_kazma@hotmail.com>
2018-01-10 01:54:39 -0500

    better cart system

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 /*
 	script file for the website 
 */
+
+$(document).ready(function()
+{
+	let myDiv = $('.prices');
+	let mySpan = $('#myTotal');
+
+	let total = 0;
+
+	for(i = 0; i < myDiv.length; i++)
+	{
+		total += parseInt(myDiv[i].innerHTML);
+	}
+	
+	mySpan.text(total);
+});
