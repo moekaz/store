4f02e6c80130736c5ea62b395184be536308de18
moekaz <mohamed.kazma@carleton.ca>
2018-03-29 23:36:22 -0400

    updated total cost system to be dynamic and added a taxed total

--- js/script.js
+++ js/script.js
@@ -2,17 +2,39 @@
 	script file for the website 
 */
 
-$(document).ready(function()
+//update cart costs 
+function UpdateTotalCost()
 {
+	
 	let myDiv = $('.prices');
 	let mySpan = $('#myTotal');
-
+	let myTotal = $("#TaxedTotal");
+	let quantities = $(".quantity");	
 	let total = 0;
-
+	let tax = 0.13;
+	
+	//add up subtotal values when the quantities are updated
 	for(i = 0; i < myDiv.length; i++)
 	{
-		total += parseInt(myDiv[i].innerHTML);
+		//error checking
+		if (quantities[i].value < 0) { quantities[i].value = 0; }
+		total += parseInt(myDiv[i].innerHTML) * parseInt(quantities[i].value);
 	}
 	
+	//resettup prices here
+	let taxedTotal = Math.floor((total * (1 + tax)) * 100) / 100;
+	myTotal.text(taxedTotal);
 	mySpan.text(total);
+}
+
+//make a purchase
+function MakePurchase()
+{
+	alert("You have successfully made a purchase. Have a good day!!");
+}
+
+//load relevant details	on load		
+$(document).ready(function()
+{
+	UpdateTotalCost();
 });
